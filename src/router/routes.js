69bd8be09d2fa01c439a67ab459edabb8d59a996
xiaohongguang <2641344956@qq.com>
2022-03-24 00:25:36 +0800

    完成了数据报表功能的开发

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,6 +9,7 @@ import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 export default [
   {
@@ -33,6 +34,7 @@ export default [
       { path: '/goods', component: List },
       { path: '/goods/add', component: Add },
       { path: '/orders', component: Order },
+      { path: '/reports', component: Report },
     ],
   },
 ]
